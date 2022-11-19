dc2cfd967d4ea9f7582bf4e54ec09d96805b7961
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-11-19 16:45:51 +0300

    Update Program.cs

--- 6_Collections5/Program.cs
+++ 6_Collections5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace _6_Collections5
@@ -7,47 +7,13 @@ namespace _6_Collections5
     {
         static void Main(string[] args)
         {
-            List<string> numbers = new();
-
-            StringAddCollection(numbers);
-
-            SortNumbers(numbers);
-
-            EliminationRepetitions(numbers);
-        }
-
-        static void StringAddCollection(List<string> numbers)
-        {
+            HashSet<string> sortNumbers = new();
             string[] listNumbers = new string[] { "50", "30", "10", "20" };
             string[] followingListNumbers = new string[] { "50", "60", "20", "10" };
-            numbers.AddRange(listNumbers);
-            numbers.AddRange(followingListNumbers);
+            sortNumbers.UnionWith(listNumbers);
+            sortNumbers.UnionWith(followingListNumbers);
 
-            Console.Write("Числа: ");
-
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                Console.Write(numbers[i] + " ");
-            }
-        }
-
-        static void SortNumbers(List<string> numbers)
-        {
-            numbers.Sort();
-
-            Console.Write("\nСортировка чисел: ");
-
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                Console.Write(numbers[i] + " ");
-            }
-        }
-
-        static void EliminationRepetitions(List<string> numbers)
-        {
-            HashSet<string> sortNumbers = new(numbers);
-
-            Console.Write("\nИсключение повторений: ");
+            Console.Write("Исключение повторений: ");
 
             foreach (string number in sortNumbers)
             {
